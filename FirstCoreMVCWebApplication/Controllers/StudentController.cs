0d41cb09a280218e7f7c055017d6fc982eee3a06
agupta800 <awadheshg577@gmail.com>
2023-12-13 11:42:15 +0545

    Add project files.

---
+++ FirstCoreMVCWebApplication/Controllers/StudentController.cs
@@ -0,0 +1,44 @@
+﻿using FirstCoreMVCWebApplication.Models;
+using FirstCoreMVCWebApplication.View_Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstCoreMVCWebApplication.Controllers
+{
+    public class StudentController : Controller
+    {
+        public ViewResult Details()
+        {
+            //Student Basic Details
+            Student student = new Student()
+            {
+                StudentId = 101,
+                Name = "Dillip",
+                Branch = "CSE",
+                Section = "A",
+                Gender = "Male"
+            };
+
+            //Student Address
+            Address address = new Address()
+            {
+                StudentId = 101,
+                City = "Mumbai",
+                State = "Maharashtra",
+                Country = "India",
+                Pin = "400097"
+            };
+
+            //Creating the View model
+            StudentDetailsViewModel studentDetailsViewModel = new StudentDetailsViewModel()
+            {
+                Student = student,
+                Address = address,
+                Title = "Student Details Page",
+                Header = "Student Details",
+            };
+
+            //Pass the studentDetailsViewModel to the view
+            return View(studentDetailsViewModel);
+        }
+    }
+}
---
+++ FirstCoreMVCWebApplication/View Model/StudentDetailsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using FirstCoreMVCWebApplication.Models;
+
+namespace FirstCoreMVCWebApplication.View_Model
+{
+    public class StudentDetailsViewModel
+    {
+        public Student? Student { get; set; }
+
+        public Address? Address { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Header { get; set; }
+
+
+
+
+    }
+}
